28e975df2d17fd40aeefb5770df0ad6ea81d455e
usuario <usuario.para.espacio1@gmail.com>
2019-05-21 21:10:13 -0500

    Seguimiento Angular, casa, martes 21/05/2019 09:10pm

--- angular/angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ angular/angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -3,18 +3,17 @@ import { Hero } from '../hero';
 import { HeroService } from "../hero.service";
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
 })
-export class HeroesComponent implements OnInit {
-  heroes: Hero[];
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
 
   constructor(
     private heroService: HeroService,
 
-  ) { 
-  }
+  ) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -23,10 +22,9 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(
-        (heroes: Hero[]) => this.heroes = heroes,
+        (heroes: Hero[]) => this.heroes = heroes.slice(1, 5),
         (err: any) => console.error(err)
       );
   }
-
 
 }
---
+++ angular/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { ActivatedRoute } from "@angular/router";
+
+import { Location } from "@angular/common";
+
+import { Hero } from "../hero";
+
+import { HeroService } from "../hero.service";
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.scss']
+})
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location,
+    
+  ) { }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(
+        (hero: Hero) => this.hero = hero,
+        (err: any) => console.error(err)
+      );
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+}
